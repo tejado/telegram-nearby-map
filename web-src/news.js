22ec4836e032feacf1b87c845cc3c16deb7efb95
SupremoIntra <daniele.intra@studenti.unimi.it>
2022-05-19 13:37:45 +0200

    19.05

---
+++ web-src/news.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const options = {
+    method: 'GET',
+    url: 'https://ukraine-war-live.p.rapidapi.com/news',
+    headers: {
+        'X-RapidAPI-Host': 'ukraine-war-live.p.rapidapi.com',
+        'X-RapidAPI-Key': '6d69d8a130msh4e3dbbae37f32a7p14a751jsn15e3cf17b9a9'
+    }
+};
+
+axios.request(options).then(function(response) {
+    console.log(response.data);
+}).catch(function(error) {
+    console.error(error);
+});
